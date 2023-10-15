4a8d2e7c96cf9606ace6387b273aa6856f39307e
Nikolai <nicolas.gordienko@gmail.com>
2023-10-15 15:42:58 +0200

    feat: adopted navbar for mobile

--- rfu/settings.py
+++ rfu/settings.py
@@ -100,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator", },
 ]
 
+CSRF_TRUSTED_ORIGINS = ['https://rfu-production.up.railway.app']
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
