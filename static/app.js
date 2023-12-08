f3aee1cb2cb5d4d56eb09c784aa6dddbdeead983
Nikolai <nicolas.gordienko@gmail.com>
2023-12-08 20:02:33 +0100

    feat: added google analytics

--- static/app.js
+++ static/app.js
@@ -67,3 +67,12 @@ manageCookiesButton.addEventListener('click', function() {
     cookieManagementModal.show();
 });
 
+// ----------------------------------------------------
+// Код для работы с google analytics
+// ----------------------------------------------------
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-EJFM51JQ57');
