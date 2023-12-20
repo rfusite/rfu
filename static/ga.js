b25436900a620ba01ad51fc022f8234a27a1b41f
Nikolai <nicolas.gordienko@gmail.com>
2023-12-21 00:47:13 +0100

    feat: added ga actions

--- static/ga.js
+++ static/ga.js
@@ -1,8 +1,19 @@
 // ----------------------------------------------------
 // Код для работы с google analytics
 // ----------------------------------------------------
+
+// Инициализация dataLayer и gtag
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
+// Настройка конфигурации Google Analytics
 gtag('config', 'G-EJFM51JQ57');
+
+// Функция для отслеживания кликов на кнопках
+function trackButtonEvent(action, category, label) {
+    gtag('event', action, {
+        'event_category': category,
+        'event_label': label
+    });
+}
